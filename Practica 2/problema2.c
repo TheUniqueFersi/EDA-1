f92774ec866bfb42168d4062e7096bf660926153
TheUniqueFersa <holafersa@gmail.com>
2024-02-22 21:28:17 -0600

    feat: añade archivos problemas 1 y 2

---
+++ Practica 2/problema2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define MAX 50
+
+lectura (int v[], int * n){
+    *n = 0;
+
+}
+
+//Funcion de entrada de datos Entrada de datos: 
+
+//funcion para recorrer todo el arreglo en busqueda de un elemento
+
+//funcion para 
+
+
+int main (void){
+    int numeros[MAX];
+
+    return 0;
+}
