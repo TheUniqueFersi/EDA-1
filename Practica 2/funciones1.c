034626dd3ab5e47e8889b86bdcf4d028c1ca4f38
TheUniqueFersa <holafersa@gmail.com>
2024-02-27 11:35:35 -0600

    añade archivo funciones para problema 1

---
+++ Practica 2/funciones1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void imprimirResultado(int arr[], int izquierda, int derecha, int indiceMax, int numVal){
+    int k;
+    printf("\t==RESULTADOS==\nDel arreglo proporcionado, hay %d elemento%c que esta%c en el rango de (%d-%d)\n", numVal, (numVal==1)? '\0':'s', (numVal==1)? '\0':'n', 10, 100);
+    if(numVal>0){
+        for(k=0; k<indiceMax; k=k+1)
+            if(arr[k]>izquierda && arr[k]<derecha)
+                printf("arr[%d]: %d\n", k, arr[k]);
+    }
+}
+void buscarRangoInt(int v[], int izquierda, int derecha, int indiceMax, int * contadorVerdaderos){
+    int k;
+    for(k=0;k<indiceMax;k=k+1){
+        if(v[k]>izquierda && v[k]<derecha)
+            *contadorVerdaderos=*contadorVerdaderos+1;
+    }
+}
