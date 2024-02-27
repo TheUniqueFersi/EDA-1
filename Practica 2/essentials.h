b24a76bb3f9e5b0a535d41ceee80074afe257d02
TheUniqueFersa <holafersa@gmail.com>
2024-02-27 10:17:33 -0600

    feat: añade archivos h con prototipos

---
+++ Practica 2/essentials.h
@@ -0,0 +1,5 @@
+#define MAXI 50
+void desplegarMenu(int seccion, char * nombre, char * explicacion);
+int leerEntero(char * indicacionEntrada);
+void llenarArregloInt(int arr[], int * nElem);
+int busquedaElemento(int v[], int elemento, int * indice, int elementosEnArreglo, char * mensajeCoincidencia);
---
+++ Practica 2/funciones1.h
@@ -0,0 +1,2 @@
+void imprimirResultado(int[], int, int, int, int);
+void buscarRangoInt(int[], int, int, int, int * );
