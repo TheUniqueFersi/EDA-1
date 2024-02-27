4515c21845230f067ff2dc4ee2369e9e6449a68f
TheUniqueFersa <holafersa@gmail.com>
2024-02-27 11:36:28 -0600

    feat: añade primer version incompleta de funciones para problema 2

--- Practica 2/problema1.c
+++ Practica 2/problema1.c
@@ -1,30 +1,13 @@
 #include <stdio.h>
-#include "essentials.c"
-
-void imprimirResultado(int numVal, int arr[], int m){
-    int k;
-    printf("\t==RESULTADOS==\nDel arreglo proporcionado, hay %d elemento%c que esta%c en el rango de (%d-%d)\n", numVal, (numVal==1)? '\0':'s', (numVal==1)? '\0':'n', 10, 100);
-    if(numVal>0){
-        for(k=0; k<m; k=k+1)
-            if(arr[k]>10 && arr[k]<100)
-                printf("arr[%d]: %d\n", k, arr[k]);
-    }
-
-}
-void buscarRangoInt(int v[], int izquierda, int derecha, int indiceMax, int * contadorVerdaderos){
-    int k;
-    for(k=0;k<indiceMax;k=k+1){
-        if(v[k]>izquierda && v[k]<derecha)
-            *contadorVerdaderos=*contadorVerdaderos+1;
-    }
-}
+#include "funciones1.h"
+#include "essentials.h"
 int main (void){
     int num[MAXI], indice, nNumerosValidos=0;
     desplegarMenu(1, "L PROBLEMA 1: =NUMEROS (10,100)=", "Identifica los numeros que se encuentran en el rango de (10-100) de un arreglo dado");
     llenarArregloInt(num, &indice);
     if(indice>0){
         buscarRangoInt(num, 10, 100, indice, &nNumerosValidos);
-        imprimirResultado(nNumerosValidos, num, indice);
+        imprimirResultado(num, 10, 100, indice, nNumerosValidos);
     } else {
         printf("El arreglo es muy pequeño, vuelve a intentar\n");
     }
--- Practica 2/problema2.c
+++ Practica 2/problema2.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
-#define MAX 50
+#include "essentials.h"
 
 lectura (int v[], int * n){
     *n = 0;
 
 }
 
-//Funcion de entrada de datos Entrada de datos: 
+/*
+    lea mientras verifica que no esten repetidos
 
-//funcion para recorrer todo el arreglo en busqueda de un elemento
+    pregunte numero que desea buscar
 
-//funcion para 
+    le diga si es enumero esta o no, si esta entonces le dice en que posicion esta
+
+    La busqueda se repite hasta que el usuario introduzca un valor menor o igual a cero
+
+*/
+
+void llenarArregloNoRepetido(int v[], int * indice){
+    int indiceFalso = 0, *ptrFalso = &indiceFalso;
+    *indice = 0;
+    do{
+        do{
+            *ptrFalso = 0;
+            v[*indice] = leerEntero("Ingresa un numero entero, cuida que sea repetido: ");
+        }while(busquedaElemento(v, v[*indice], &indiceFalso, *indice, "Este numero ya se ha ingresado previamente, ingresa uno diferente: ")>0);
+        *indice=*indice+1;
+    }while(*indice<MAXI && v[*indice-1] > 0);
+    if(*indice>=MAXI)
+        printf("------// Has alcanzado el limite de valores para un arreglo\n");
+    *indice=*indice-1;
+}
 
 
 int main (void){
-    int numeros[MAX];
-
+    int listaNoRepetidos[MAXI], indice, maxIndiceValido, valorBusqueda;
+    desplegarMenu(1, "L PROBLEMA 2: =BUSQUEDA DE ELEMENTO EN LISTA NO REPETIDA=", "Identifica si el numero proporcionado esta o no en el arreglo, si esta, indica en que indice");
+    llenarArregloNoRepetido(listaNoRepetidos, &indice);
+    if(indice>0){
+        
+        
+        while()//para repetir numero que busque
+    } else {
+        printf("El arreglo es muy pequeño, vuelve a intentarlo\n");
+    }
     return 0;
 }
