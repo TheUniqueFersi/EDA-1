4bf307aeb88490445c9d2f1a4fbc5ec7db5cda1b
TheUniqueFersa <holafersa@gmail.com>
2024-02-28 00:18:34 -0600

    fix(problema 2): arregla impresión y añade indices en impresion

--- Practica 2/problema2.c
+++ Practica 2/problema2.c
@@ -16,7 +16,7 @@ void llenarArregloNoRepetido(int v[], int * indice){
     do{
         do{
             *ptrFalso = 0;
-            v[*indice] = leerEntero("Ingresa un numero entero, cuida que sea repetido: ");
+            v[*indice] = leerEntero("Ingresa un numero entero, cuida que no sea repetido: ");
         }while(busquedaElemento(v, v[*indice], &indiceFalso, *indice, "Este numero ya se ha ingresado previamente, ingresa uno diferente: ")>0);
         *indice=*indice+1;
     }while(*indice<MAXI && v[*indice-1] > 0);
@@ -27,14 +27,16 @@ void llenarArregloNoRepetido(int v[], int * indice){
 
 
 int main (void){
-    int listaNoRepetidos[MAXI], indice, valorBusqueda, indiceFalso;
+    int listaNoRepetidos[MAXI], indice, valorBusqueda, indiceCoincidencia;
     desplegarMenu(1, "L PROBLEMA 2: =BUSQUEDA DE ELEMENTO EN LISTA NO REPETIDA=", "Identifica si el numero proporcionado esta o no en el arreglo, si esta, indica en que indice");
     llenarArregloNoRepetido(listaNoRepetidos, &indice);
     if(indice>0){        
         while((valorBusqueda=leerEntero("Ingresa un numero que quieras buscar en la lista previamente creada: "))>0){
-            indiceFalso=0;
-            if((busquedaElemento(listaNoRepetidos, valorBusqueda, &indiceFalso, indice, "Este elemento SI se encuentra en la lista previamente dada\n"))<0)
+            indiceCoincidencia=0;
+            if((busquedaElemento(listaNoRepetidos, valorBusqueda, &indiceCoincidencia, indice, "Este elemento SI se encuentra en la lista previamente dada\n"))<0)
                 printf("Este elemento NO se encuentra en el arreglo perviamente dado\n");
+            else
+                printf("El elemento se encuentra en el indice: [%d] = %d\n", indiceCoincidencia-1, valorBusqueda);
         }
     } else {
         printf("El arreglo es muy pequeño, vuelve a intentarlo\n");
