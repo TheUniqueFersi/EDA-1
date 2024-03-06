be20702c7ec08e1a9eb81a2077f3e9b2b4eb1f9a
TheUniqueFersa <holafersa@gmail.com>
2024-03-05 22:11:07 -0600

    feat(stoi): añade funcion completamente funcional

--- Practica 3/stringManipulation.c
+++ Practica 3/stringManipulation.c
@@ -6,7 +6,7 @@
 int strlenMX(char * s){//Es lo mismo que 'char s[]'
     int n;
     n=0;
-    while(s[n]!=0)
+    while(s[n]!=0 && s[n]!='\n')
         n=n+1;    
     return n;
 }
@@ -38,27 +38,68 @@ void stringToUpperOrLower(char * cadena, int caso){ //caso>=1
         printf("Tu cadena esta vacia\n");
 }
 
+int isANumber(char * cadena){
+    int esUnNumero, k=1;
+    esUnNumero = (cadena[0]=='-')? -1:1;
+    while(cadena[k]!='\0' && cadena[k]!='\n'){
+        if(!(cadena[k] >= '0' && cadena[k] <= '9'))
+            esUnNumero = 0;
+        k++;
+    }
+    return esUnNumero;
+}
+
+int ctoi(char caracter){
+    return caracter - 48;
+}
+
 int stoi(char * cadena){//stringToInt
-    int longitud, k, numeroEntero, maxIntPosible;
+    int longitud, k, numeroEntero, maxIntPosible, iInicioNumero;
     longitud=strlenMX(cadena);
     k=0;
-    printf("long: %d\n", longitud);
-    printf("%d\n", (int)pow(2,8*sizeof(int)));
+    numeroEntero=0;
+    iInicioNumero=0;
+    // printf("long: %d\n", longitud);
+    // printf("%d\n", (int)pow(2,8*sizeof(int)));
     maxIntPosible = ((int)pow(2,8*sizeof(int)));//Hal alguna forma de saber si una compu no utiliza 8 bits?
     printf("MAX: %d\n", maxIntPosible);
     //maxIntPosible/=2;
     //printf("%d\n", maxIntPosible);
-    if(longitud>0){
-        printf("%f\n", pow(10, (longitud-1)));
-        while(cadena[k] != 0){
-            if(pow(10, (longitud-1)) > maxIntPosible)
-                printf("WTFFFD");
-
+    int ciclos, ciclosN, p=0;
+    if(longitud>0 && isANumber(cadena)!=0){
+        //printf("%f\n", pow(10, (longitud-1)));
+        if(isANumber(cadena)==-1){
+            iInicioNumero = 1;
+        }            
+        do{
+            ciclos = 0;
+            ciclosN = (ctoi(cadena[longitud-1]))*10;
+            printf("ciclosN: %d\n", ciclosN);
+            if(p!=0){
+                do{
+                    printf("DEP: valor de sumas%d\n", (int)pow(10, p-1));
+                    numeroEntero = numeroEntero + ((int)pow(10, p-1));
+                    ciclos++;
+                }while(ciclos<ciclosN && numeroEntero>0);
+            } else {
+                do{
+                    printf("DEP: valor de sumas%d\n", (int)pow(10, p-1));
+                    numeroEntero = numeroEntero + 1;
+                    ciclos+=10;
+                }while(ciclos<ciclosN && numeroEntero>0);
+            } 
             
-            k++;
-        }
-        
+            printf("LOL: %d\n", numeroEntero);
+            p++;
+            longitud--;
+        }while(longitud!=iInicioNumero && numeroEntero>0);
+        if(numeroEntero<0)
+            numeroEntero = maxIntPosible;
+        else
+            if(isANumber(cadena)==-1)
+                numeroEntero*=-1;
     }
+    return numeroEntero;
 } 
 //2147483647 -> numero maximo
 
