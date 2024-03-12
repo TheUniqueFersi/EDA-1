6e7cc926975fe0d6e79a6c0307c99ff7e67c07b6
TheUniqueFersa <holafersa@gmail.com>
2024-03-11 23:26:44 -0600

    feat(main.c): prototipos para funciones solicitadas

--- MemoriaDinamica/main.c
+++ MemoriaDinamica/main.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+void lee(int *n);
+
+void suma(int *n1, int *n2, int *res);
+void resta(int n1, int *n2, int *res);
 int main(void){
-    // Se reserva memoria para un entero
-    int *direccion_numero = malloc(sizeof(int));
-
-    //Se verifica si la asignacion de memoria fue exitosa
-    if(direccion_numero == NULL){
-        printf("Error: No se pudo asignar memoria. \n");
-        return 1; //O:
-    }
-    //Asigna un valor al entero
-    *direccion_numero = 16;
-
-    //Direccion del apuntador
-    printf("La direccion de la variable direccion_numero es: %p\n", &direccion_numero);
-    //Direccion que guarda el apuntador
-    printf("El contenido de la variable (que es un apuntador) direccion_numero es: %p\n", direccion_numero);
-    //Se muestra el valor del entero
-    printf("El numero que almacena direccion_numero es: %d\n", *direccion_numero);
-
-    // Se libera la memoria asignada por malloc(), pues ya no la usaremos y es buena práctica liberarla
-    free(direccion_numero);
-
-    return 0;
+    int *op1, *op2, res;
+    //Con base en los valores de los números leídos, se calcula la suma y resta
+    //correspondiente y se imprime en pantalla
+    
 }
