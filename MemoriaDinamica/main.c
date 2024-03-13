0e3d950fe8a1318066b3fbf407f4e81a256e0c5d
TheUniqueFersa <holafersa@gmail.com>
2024-03-12 23:22:29 -0600

    feat(memoriaDinamica primer ejemplo): añade total funcionalidad

--- MemoriaDinamica/main.c
+++ MemoriaDinamica/main.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 void lee(int *n);
-
 void suma(int *n1, int *n2, int *res);
 void resta(int n1, int *n2, int *res);
+void lee(int *n){
+    char c; int verif;
+    do{
+        printf("Ingresa un numero entero: ");
+        fflush(stdin);
+        verif=scanf("%d", n);
+        if(verif==0){
+            while((c=getchar()) != '\n' && c != EOF);
+            printf("Se esperaba un entero, intenta de nuevo - ");
+        }
+    }while(verif==0);
+}
+void suma(int *n1, int *n2, int *res){
+    *res = *n1 + *n2;
+}
+void resta(int n1, int *n2, int *res){
+    *res = n1 - *n2;
+}
 int main(void){
     int *op1, *op2, res;
-    //Con base en los valores de los números leídos, se calcula la suma y resta
-    //correspondiente y se imprime en pantalla
-    
+    op1 = malloc(sizeof(int));
+    op2 = malloc(sizeof(int));
+    lee(op1);
+    lee(op2);
+    printf("Dados los 2 numeros proporcionados, se calcula la:\n---SUMA---\n");
+    suma(op1, op2, &res);
+    printf("%d + %d = %d\n---RESTA---\n", *op1, *op2, res);
+    resta(*op1, op2, &res);
+    printf("%d - %d = %d\n", *op1, *op2, res);
+    free(op1);
+    free(op2);
+    return 0;
 }
