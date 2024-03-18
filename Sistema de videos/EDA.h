f41a226c48d2c40316c6417ed25bd8aad19be2ea
TheUniqueFersa <holafersa@gmail.com>
2024-03-18 00:17:40 -0600

    feat(EDA.c .h): añade archivo con código util de anteriores prácticas

---
+++ Sistema de videos/EDA.h
@@ -0,0 +1,6 @@
+int* push_int(int *arr, int n);
+int verifAlloc(int *arr, char* cadena);
+void imprimir_Elem_pseudo_cola(int *pseudo, char *cadena);
+void bufferflush(); 
+int leerEntero(char * mensajeEntrada, char * mensajeError);
+int busquedaElemento(int v[], int elemento, int * indice, int elementosEnArreglo, char * mensajeCoincidencia);
